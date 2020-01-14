698f4f9eb0e7644cf7c05bb1ae0379ce13e2734a
Samuli <samuli.harjunen@gmail.com>
2020-01-14 11:17:41 +0200

    Added person class

---
+++ src/fi/academy/Person.java
@@ -0,0 +1,31 @@
+package fi.academy;
+
+public class Person {
+    private String name;
+    private int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+}
+
+    public Person() {
+        this.name = "";
+        this.age = 0;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+
+    }
+
+    public String toString() {
+    return "Name: " + this.name + ", Age: " + this.age;
+    }
